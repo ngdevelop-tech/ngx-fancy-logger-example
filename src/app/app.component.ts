9b188b0fa163fc1b4857874e8e23e6020e32f7d9
Ankit.P <ankit.p@ezdi.us>
2020-05-17 15:29:46 +0530

    Ngx-Fancy-Logger Example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
-
+import { NgxFancyLoggerService, LogLevel } from 'ngx-fancy-logger';
+import { interval } from 'rxjs';
+import { take, map } from 'rxjs/operators';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ngx-fancy-logger-example';
+  
+  constructor(private logger: NgxFancyLoggerService){
+    logger.header('App Component Loaded...')
+  }
+  
+  showHeaderLog(){
+    this.logger.header('NgxFancyLogger Header Log with configured color and fontSize', {color: 'red', fontSize: 30})
+  }
+
+  showDifferentLogLevels(){
+    this.logger.info('This is a info log', {data: {a: 20, b:30 }});
+    this.logger.debug('This is a debug log', {data: {a: 20, b:30 }});
+    this.logger.warning('This is a warning log', {data: {a: 20, b:30 }});
+    this.logger.error('This is a error log', {data: {a: 20, b:30 }});
+  }
+
+  debugRxJSStream(){
+    let source$ = interval(1000);
+    source$.pipe(
+      this.logger.debugOperator(),
+      map<number, number>(data => data*100),
+      this.logger.debugOperator('Result', LogLevel.INFO),
+      take(5),
+    ).subscribe();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { NgxFancyLoggerModule } from 'ngx-fancy-logger';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxFancyLoggerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
